971788f8d7fdcb8a35d4021cfbf2447010cee8fc
ltoker <lilchpilch@yahoo.com>
2017-11-27 15:03:49 -0800

    For now contains pathes and functions

---
+++ SetUp.R
@@ -0,0 +1,22 @@
+#Set up the path for general scripts
+GenScriptPath = "/home/ltoker/Rscripts/"
+
+#Set up the path for project specific scripts
+ProjScriptPath = "ProjectScripts/"
+
+#Set up the path for results directory
+if(!"GeneralResults" %in% list.dirs(full.names = FALSE)){
+  dir.create("GeneralResults")
+}
+GeneralResultsPath = 'GeneralResults/'
+
+# Source required functions
+source(paste0(GenScriptPath,"general_functions.R"))
+source(paste0(ProjScriptPath,"projectFunc.R"))
+
+packageF("sva")
+packageF("gplots")
+packageF("scales")
+select = dplyr::select
+filter = dplyr::filter
+mutate = dplyr::mutate
